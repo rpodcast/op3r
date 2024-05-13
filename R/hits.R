af5c45dfa25d28573c18548204fe98541f9f15c7
Eric Nantz <thercast@gmail.com>
2024-05-12 21:41:33 -0400

    begin hits endpoint function

---
+++ R/hits.R
@@ -0,0 +1,4 @@
+op3_hits <- function(format = "tsv", limit = 100, start = NULL, startAfter = NULL, end = NULL, url = NULL, hashedIpAddress = NULL) {
+  assert_valid_limit(limit, max_limit = 1000)
+  # TODO finish
+}
