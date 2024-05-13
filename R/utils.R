c6f3fca1ceea2fc721ce278d98b71f8c454b18e9
Eric Nantz <thercast@gmail.com>
2024-05-12 20:18:51 -0400

    add functions to check and verify show ID types

--- R/utils.R
+++ R/utils.R
@@ -19,3 +19,32 @@ is_rss_url <- function(x) {
 is_podcast_guid <- function(x) {
   stringr::str_detect(x, "^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$")
 }
+
+is_show_uuid <- function(x) {
+  !stringr::str_starts(x, "http") & (stringr::str_length(x) == 32)
+}
+
+get_id_type <- function(x) {
+  if (!any(is_rss_url(x), is_podcast_guid(x), is_show_uuid(x))) {
+    cli::cli_abort(
+      message = c(
+        "show_id value does not appear to match a show UUID, podcast GUID, or RSS feed URL;"
+      ),
+      call = rlang::caller_env()
+    )
+  }
+  if (is_rss_url(x)) return("rss")
+  if (is_podcast_guid(x)) return("podcast_guid")
+  if (is_show_uuid(x)) return("show_uuid")
+}
+
+assert_valid_limit <- function(limit, max_limit = 20000) {
+  if (limit > max_limit) {
+    cli::cli_abort(
+      message = c(
+        "maximum limit value supported for this function is {.var {max_limit}}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
