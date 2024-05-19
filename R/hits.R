258a4e94db2791f17dc055e472dac1fa622d5e5e
Eric Nantz <thercast@gmail.com>
2024-05-19 00:12:08 -0400

    add URL param examples for hits endpoint

--- R/hits.R
+++ R/hits.R
@@ -17,8 +17,10 @@
 #' @param end Optional date or date-time object to filter results to those
 #'   ending at the supplied value. The object must be class `Date`. Default
 #'   is `NULL`.
-#' @param url Optional string to fulter results by the specific URL providing
-#'   the redirect. Default is `NULL`.
+#' @param url Optional string to filter results by the specific URL providing
+#'   the redirect. The URL is often a direct link to a podcast episode file,
+#'   or a version of the URL with a wildcard character `*` to enable multiple
+#'   episodes for a given podcast. Default is `NULL`.
 #' @param hashed_ip_address Optional string to filter results by the specific
 #'   IP address secure hash. Default is `NULL`.
 #' 
@@ -28,7 +30,14 @@
 #' @examplesIf op3r::op3_token_isset()
 #' # Requires API token
 #'
+#' # obtain 10 records
 #' op3_hits(limit = 10)
+#' 
+#' # records based on specific podcast episode
+#' op3_hits(url = "https://op3.dev/e/serve.podhome.fm/episode/99cfd30f-e40c-426a-3557-08dc4ea63bb0/63851340872787683054574074-0a1f-4951-8f2a-9084678c6604v1.mp3")
+#' 
+#' # records for multiple episodes using a wildcard in URL
+#' op3_hits(url = "https://op3.dev/e/serve.podhome.fm/episode/99cfd30f-e40c-426a-3557-08dc4ea63bb0/*")
 op3_hits <- function(format = "json", limit = 100, desc = TRUE, start = NULL, start_inclusive = TRUE, end = NULL, url = NULL, hashed_ip_address = NULL) {
   assert_valid_limit(limit, max_limit = 1000)
   
