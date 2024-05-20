bb16ed4a5a5f8314fd767a27204773974e8efebd
Eric Nantz <thercast@gmail.com>
2024-05-19 22:53:18 -0400

    fix typo in op3 api token error

--- R/credentials.R
+++ R/credentials.R
@@ -15,7 +15,7 @@ op3_token_isset <- function() {
 #' @export
 get_op3_token <- function() {
   if (!op3_token_isset()) {
-    cli::cli_abort("Unable to find OP3 API token. Please create a developer account at {.url https://op3.dev/api/keys} and set {.envvar OP3_TOKEN} environment variable inside your {.file ~/.Renviron} file.")
+    cli::cli_abort("Unable to find OP3 API token. Please create a developer account at {.url https://op3.dev/api/keys} and set {.envvar OP3_API_TOKEN} environment variable inside your {.file ~/.Renviron} file.")
   } else {
     return(
       list(
